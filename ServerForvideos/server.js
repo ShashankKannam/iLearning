59bbca72e3b763876633d541b40d2185e1b06516
Shashank kannam <kannamshashank19@gmail.com>
2017-04-01 23:28:51 -0400

    Added posting comments feature to server

--- ServerForvideos/server.js
+++ ServerForvideos/server.js
@@ -32,7 +32,30 @@ var tutorials = [{id:1,
                                                 description:"Lecture 1: Introduction to iOS 10, Xcode 8 and Swift 3",
                                                 iframe:'<div class="container"><iframe class="video" src="https://www.youtube.com/embed/ilQ-tq772VI?list=PLPA-ayBrweUz32NSgNZdl0_QISw-f12Ai" frameborder="0" allowfullscreen></iframe>',
                                                thumbnail:"https://pbs.twimg.com/profile_images/520246535632592896/N2cIW5h2.png"}
-                             ];
+                ];
+
+
+var comments = [{
+  username:"Abhra",
+  comment:"Thanks for sharing this very helpful video."
+}];
+
+app.post('/comments', function(req,res){
+  var comment = req.body;
+if (comment){
+  if (comment.username && comment.comment){
+    comments.push(comment);
+  } else {
+    console.log(comments)
+    res.send("You posted invalid data.");
+  }
+} else {
+      res.send("You post has no body.");
+}
+    console.log(comments);
+    res.send("Your comment is succesfully posted.");
+});
+
 
 app.get('/tutorials', function(req,res){
   console.log("GET from Server");
